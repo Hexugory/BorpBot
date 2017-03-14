bbff3b4b894762660c5cb81851973656e35eaf2c
Hexugory <5439333+Hexugory@users.noreply.github.com>
2017-03-14 18:52:03 -0500

    fixed permission causing edgifier to error

--- commands/meme/edgifier.js
+++ commands/meme/edgifier.js
@@ -22,7 +22,7 @@ module.exports = class AnnounceToggleCommand extends commando.Command {
 			return true;
 		}
 		else{
-			return client.provider.get(msg.guild, 'memeChannelIDs', null).indexOf(msg.channel.id) > -1
+			return msg.client.provider.get(msg.guild, 'memeChannelIDs', null).indexOf(msg.channel.id) > -1
 		}
 	}
 
