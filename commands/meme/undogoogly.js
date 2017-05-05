86a813c9aba00c965663c4df1bf0a9bcf06bf8f3
jimmyy4 <jimmy@jpmiii.com>
2017-05-05 15:28:18 -0500

    e v e r y t i m e

--- commands/meme/undogoogly.js
+++ commands/meme/undogoogly.js
@@ -1,5 +1,6 @@
 const commando = require('discord.js-commando');
 const sqlite = require('sqlite');
+const mMessages = require('../../perms.js').mMessages;
 
 module.exports = class UndoGooglyCommand extends commando.Command {
 	constructor(client) {
