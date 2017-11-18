76a0b8f31010ae4c48eecf71e09f043ef45c7e5e
jimmyy4 <jimmy@jpmiii.com>
2017-11-17 22:13:47 -0600

    danker profiles

--- commands/util/viewprofile.js
+++ commands/util/viewprofile.js
@@ -1,4 +1,5 @@
 const commando = require('discord.js-commando');
+const discord = require('discord.js');
 const sqlite = require('sqlite');
 const path =  require('path');
 const oneLine = require('common-tags').oneLine;
@@ -24,12 +25,20 @@ module.exports = class ViewProfileCommand extends commando.Command {
 	}
 
 	async run(msg, args) {
-		var profiles = this.client.provider.get(msg.guild, 'profiles', {});
-		if(!profiles[args.mb.id]){
+		let profiles = this.client.provider.get(msg.guild, 'profiles', {settings: {fields: []}});
+		let profileEmbed = new discord.RichEmbed();
+		if(typeof profiles[args.mb.id] != 'object'){
 			return msg.reply(`That member does not have a profile.`);
 		}
 		else{
-			return msg.channel.send(`${args.mb.user.username}'s profile: ${profiles[args.mb.id]}`);
+			profileEmbed.setAuthor(`${args.mb.user.username}'s profile`, args.mb.user.avatarURL);
+			profileEmbed.setDescription(profiles[args.mb.id].description ? profiles[args.mb.id].description : '<unset>');
+			profileEmbed.setColor(profiles[args.mb.id].color ? profiles[args.mb.id].color : [114, 137, 218]);
+			profiles[args.mb.id].thumbnail ? profileEmbed.setThumbnail(profiles[args.mb.id].thumbnail) : null;
+			for(var i = 0; i < profiles.settings.fields.length; i++){
+				profileEmbed.addField(profiles.settings.fields[i], profiles[args.mb.id][profiles.settings.fields[i]] ? profiles[args.mb.id][profiles.settings.fields[i]] : '<unset>', true);
+			}
+			return msg.channel.send({embed: profileEmbed});
 		}
 	};
 }
