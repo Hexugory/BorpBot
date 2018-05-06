076411051c005dfb6c3b85885cdd28ce5333abda
Hexugory <5439333+Hexugory@users.noreply.github.com>
2018-05-05 23:03:50 -0500

    unrestricted suggest from DMs added examples there were more things i wanted to do but i actually forgot

--- commands/suggestion/suggest.js
+++ commands/suggestion/suggest.js
@@ -11,12 +11,9 @@ module.exports = class SuggestCommand extends commando.Command {
 			memberName: 'suggest',
 			description: oneLine`Sends a suggestion to the staff for future viewing.
 			Can send anonymously. However if you do send anonymously, the staff can view who you are, but you will be notified if they do so.
-			Can only be used in DMs.
-			Reccomended that you only send "'suggest" and answer the prompts one by one.
-			You can send all of the arguments in one message, but only do so if you know what you're doing.
 			Suggestions are limited to 1900 characters.
 			Joke suggestions may lead to being blacklisted and/or other mod action.`,
-			examples: ['\'suggest'],
+			examples: ['\'suggest "BorpBot Testing" no BorpBot more.', '\'suggest "BorpBot Testing" "yes" "BorpBot more."', '\'suggest Server no BorpBot more.'],
 
 			args: [
 				{
@@ -54,47 +51,41 @@ module.exports = class SuggestCommand extends commando.Command {
 		function generateID(len){
 			let id = Math.floor(Math.random()*Math.pow(10, len));
 			if(suggestions.find(function(element){return element.id === id})){
-				return generateID();
+				return generateID(len);
 			}
 			else{
 				return id.toString(10);
 			}
 		}
-		if(msg.channel.guild != undefined){
-			msg.author.send(`You can only use 'suggest in DMs.\nPlease use \`'help suggest\`.`);
+		var suggestionChannels = this.client.provider.get(args.gi, 'suggestionChannelIDs', false);
+		if(!suggestionChannels){
+			msg.reply("That server does not have suggestions enabled.");
 		}
 		else{
-			var suggestionChannels = this.client.provider.get(args.gi, 'suggestionChannelIDs', false);
-			if(!suggestionChannels){
-				msg.reply("That server does not have suggestions enabled.");
+			args.gi.members.fetch(msg.author).then(author => {
+			if((this.client.provider.get(args.gi, 'blacklist', {}).suggest && this.client.provider.get(args.gi, 'blacklist', {}).suggest.includes(msg.author.id))
+			|| (this.client.provider.get(args.gi, 'blacklist', {}).server && this.client.provider.get(args.gi, 'blacklist', {}).server.includes(msg.author.id))
+			|| author === undefined){
+				msg.reply("You cannot suggest to that server.");
 			}
 			else{
-				args.gi.members.fetch(msg.author).then(author => {
-				if((this.client.provider.get(args.gi, 'blacklist', {}).suggest && this.client.provider.get(args.gi, 'blacklist', {}).suggest.includes(msg.author.id))
-				|| (this.client.provider.get(args.gi, 'blacklist', {}).server && this.client.provider.get(args.gi, 'blacklist', {}).server.includes(msg.author.id))
-				|| author === undefined){
-					msg.reply("You cannot suggest to that server.");
+				if(args.sg.length > 1900){
+					msg.reply("Your suggestion cannot be longer than 1900 characters.");
 				}
 				else{
-					if(args.sg.length > 1900){
-						msg.reply("Your suggestion cannot be longer than 1900 characters.");
-					}
-					else{
-						var fromstr = args.an ? "[Anonymous]" : msg.author.username;
-						suggestions.push({
-							suggestion: args.sg,
-							user: msg.author.id,
-							anonymous: args.an,
-							id: generateID(16)
-						});
-						console.log(suggestions[suggestions.length-1].id)
-						sendMessages(suggestionChannels, `${fromstr} suggested: ${args.sg}\nSuggestion ID: ${suggestions[suggestions.length-1].id}`);
-						this.client.provider.set(args.gi, 'suggestions', suggestions);
-						msg.reply("Suggestion sent.");
-					}
+					var fromstr = args.an ? "[Anonymous]" : msg.author.username;
+					suggestions.push({
+						suggestion: args.sg,
+						user: msg.author.id,
+						anonymous: args.an,
+						id: generateID(16)
+					});
+					sendMessages(suggestionChannels, `${fromstr} suggested: ${args.sg}\nSuggestion ID: ${suggestions[suggestions.length-1].id}`);
+					this.client.provider.set(args.gi, 'suggestions', suggestions);
+					msg.reply("Suggestion sent.");
 				}
-				});
 			}
+			});
 		}
 	};
 }
