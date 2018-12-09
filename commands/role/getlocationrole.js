c00e111b476ccbae3cf449233a17e2c3aed5bff7
jimmyy4 <jimmy@jpmiii.com>
2018-12-09 14:46:18 -0600

    less dumb

--- commands/role/getlocationrole.js
+++ commands/role/getlocationrole.js
@@ -12,7 +12,7 @@ module.exports = class GetLocationRoleCommand extends commando.Command {
 			group: 'role',
 			memberName: 'getlocationrole',
 			description: oneLine`Get a location color role.
-			Use \`'getlocationrole\` to remove your location role.
+			Use \`'getlocationrole\` with no argument to remove your location role.
 			Ratelimit of 1 per week.
 			Exclusive to Touhou Project Discord.
 			Read #rules for the role list.`,
@@ -25,7 +25,7 @@ module.exports = class GetLocationRoleCommand extends commando.Command {
 					label: 'role',
 					prompt: 'Please enter role.',
 					type: 'role',
-					default: 'none'
+					default: false
 				}
 			]
 		});
@@ -36,53 +36,25 @@ module.exports = class GetLocationRoleCommand extends commando.Command {
 	}
 
 	async run(msg, args) {
-		function checkCooldown(){
-			for(var i = 0; i < cooldownTimes.length+1; i++){
-				if(cooldownTimes[i] === undefined){
-					return true;
-				}
-				else if(cooldownTimes[i].id === msg.author.id && moment.utc().isAfter(cooldownTimes[i].time)){
-					return true;
-				}
-				else if(cooldownTimes[i].id === msg.author.id && moment.utc().isBefore(cooldownTimes[i].time)){
-					msg.reply(`You cannot use this command again until ${moment.utc(cooldownTimes[i].time).format('MMMM Do YYYY, h:mm:ss a ZZ')}, one week from when you used the command last.`);
-					return false;
-				}
-			}
-		}
-		function searchArrayForID(element){
-			return element.id === msg.author.id;
-		}
 		var roles = msg.client.provider.get(msg.guild, "locationRoles", []);
 		var cooldownTimes = msg.client.provider.get(msg.guild, "cooldownTimes", []);
-		if(args.rn === "none"){
-			for(var i = 0; i < roles.length; i++){
-				let foundLocationRole = msg.member.roles.get(roles[i]);
-				if(foundLocationRole) msg.member.roles.remove(foundLocationRole, "Requested new location role");
-			}
+		var lastUsedIndex = cooldownTimes.findIndex(element => {return element.id === msg.author.id});
+		var lastUsed = cooldownTimes[lastUsedIndex];
+		if(!args.rn){
+			msg.member.roles.set(msg.member.roles.filter(role => {return !roles.includes(role.id)}).array());
 			return msg.reply(`Role removed.`);
 		}
 		else{
-			if(checkCooldown()){
-				if(roles.includes(args.rn.id)){
-					let newRoles = msg.member.roles.filter(role => {return !roles.includes(role.id)}).array()
-					newRoles.push(args.rn)
-					msg.member.roles.set(newRoles)
-					var IDIndex = cooldownTimes.findIndex(searchArrayForID);
-					if(IDIndex>-1){
-						cooldownTimes[IDIndex].time = moment.utc().add(1, "week");
-						msg.client.provider.set(msg.guild, "cooldownTimes", cooldownTimes);
-					}
-					else{
-						cooldownTimes.push({time: moment.utc().add(1, "week"), id: msg.author.id});
-						msg.client.provider.set(msg.guild, "cooldownTimes", cooldownTimes);
-					}
-					return msg.reply(`Given role ${args.rn}.`)
-				}
-				else{
-					return msg.reply(`That's not a location role.`)
-				}
-			}
+			if(!roles.includes(args.rn.id)) return msg.reply("That's not a location role.");
+			if(msg.member.roles.get(args.rn.id)) return msg.reply("You already have that role.");
+			if(lastUsed && moment.utc().isBefore(lastUsed.time)) return msg.reply(`You cannot use this command again until ${moment.utc(lastUsed.time).format('MMMM Do YYYY, h:mm:ss a ZZ')}, \`${Math.round((parseInt(moment.utc(lastUsed.time).format('x'))-parseInt(moment.utc().format('x')))/1000/60/60)}\` hours from now.`);
+			let newRoles = msg.member.roles.filter(role => {return !roles.includes(role.id)}).array();
+			newRoles.push(args.rn)
+			msg.member.roles.set(newRoles)
+			if(lastUsed) cooldownTimes[lastUsedIndex].time = moment.utc().add(1, "week");
+			else cooldownTimes.push({time: moment.utc().add(1, "week"), id: msg.author.id});
+			msg.client.provider.set(msg.guild, "cooldownTimes", cooldownTimes);
+			return msg.reply(`Given role ${args.rn}.`)
 		}
 	};
 }
