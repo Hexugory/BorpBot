ae357775f776fc201dc10bfd64af361db54f88c3
Hexugory <5439333+Hexugory@users.noreply.github.com>
2019-05-25 14:32:30 -0500

    didnt even test this

---
+++ commands/gacha/optrollnotify.js
@@ -0,0 +1,34 @@
+const commando = require('discord.js-commando');
+const sqlite = require('sqlite');
+
+module.exports = class OptItemNotifyCommand extends commando.Command {
+	constructor(client) {
+		super(client, {
+			aliases: ['fuckurdad'],
+			name: 'optrollnotify',
+			group: 'meme',
+			memberName: 'optrollnotify',
+			description: 'Toggle notifications for when you get rolls.',
+			examples: ['\'optrollnotify'],
+			guildOnly: true
+		});
+	}
+
+	hasPermission(msg) {
+		return msg.client.isOwner(msg.author) || (msg.guild  && msg.guild.id === "163175631562080256" && (msg.member.permissions.has('MANAGE_MESSAGES') || msg.client.provider.get(msg.guild, 'memeChannelIDs', []).includes(msg.channel.id)))
+	}
+
+	async run(msg, args) {
+		var optlist = this.client.provider.get(msg.guild, 'optgachalist', []);
+		if(!optlist.includes(msg.author.id)){
+			optlist.push(msg.author.id);
+			this.client.provider.set(msg.guild, 'optgachalist', optlist);
+			return msg.reply(`You will now be notified when you get an item.`);
+		}
+		else{
+			optlist.splice(optlist.indexOf(msg.author.id), 1);
+			this.client.provider.set(msg.guild, 'optgachalist', optlist);
+			return msg.reply(`You will no longer be notified when you get an item.`);
+		}
+	}
+};
