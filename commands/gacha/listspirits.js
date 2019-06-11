92b85a8c54de5d98e1fb5a625a0ca42cf47192a4
James McCall <jimmy.mccall4@gmail.com>
2019-06-10 20:59:42 -0500

    cheap

--- commands/gacha/listspirits.js
+++ commands/gacha/listspirits.js
@@ -32,7 +32,8 @@ module.exports = class ListSpiritsCommand extends commando.Command {
 		var startIndex = 0+20*(args.page-1);
 		var endIndex = Math.min(gacha.spirits.length, startIndex+20);
 		for(var i = startIndex; i < endIndex; i++){
-			let dude = await msg.guild.members.fetch(gacha.spirits[i]).catch(err => {return 'Dead User'});
+			let dude = msg.guild.members.get(gacha.spirits[i])
+			dude = dude ? dude : await msg.guild.members.fetch(gacha.spirits[i]).catch(err => {return 'Dead User'});
 			if(dude.user) dude = dude.user.tag;
 			description += `#${i} ${dude}\n`;
 		}
