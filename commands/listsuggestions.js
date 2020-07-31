b6b7932a29c669be0fb7989e264b4f0d757b46c5
James McCall <jimmy.mccall4@gmail.com>
2020-07-31 16:45:45 -0500

    aagh

--- commands/listsuggestions.js
+++ commands/listsuggestions.js
@@ -16,8 +16,7 @@ module.exports = {
             let sender = msg.guild.members.resolve(suggestion.sender_id);
             sender = sender ? sender.user.tag : '[Missing User]';
             message += `${suggestion.anonymous ? '[Anonymous]' : sender} suggested: ${suggestion.suggestion}
-Suggestion created at ${moment.utc(suggestion.createdAt).format('ddd MMM D YYYY')}
-Suggestion ID: ${suggestion.id}\n`
+Suggestion created at ${moment.utc(suggestion.createdAt).format('ddd MMM D YYYY')}, ID: ${suggestion.id}\n`
         }
         
         if (message.length < 1) return msg.reply('this server has no suggestions');
