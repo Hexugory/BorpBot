17b97bb28d3a1c7efc77dd8a64e2c1d32c02af84
Hexugory <5439333+Hexugory@users.noreply.github.com>
2020-12-18 19:09:04 -0600

    SUG GEST

---
+++ slash/suggest.js
@@ -0,0 +1,49 @@
+const { SlashCommand, CommandOptionType } = require('slash-create');
+const { db, sendMessages } = require('../borp.js')
+const suggestions = db.import('../models/suggestions');
+const channelTags = db.import('../models/channelTags');
+
+module.exports = class SuggestCommand extends SlashCommand {
+    constructor(creator) {
+        super(creator, {
+            name: 'suggest',
+            description: 'Send a suggestion to the server.',
+            guildID: '163175631562080256',
+
+            options: [{
+                type: CommandOptionType.BOOLEAN,
+                name: 'anonymous',
+                description: 'Would you like your suggestion to be anonymous?',
+                required: true
+            },
+            {
+                type: CommandOptionType.STRING,
+                name: 'suggestion',
+                description: 'The content of your suggestion.',
+                required: true
+            }]
+        });
+    }
+
+    async run(ctx) {
+        const suggestion = await suggestions.create({
+            guild_id: ctx.guildID,
+            sender_id: ctx.member.id,
+            anonymous: ctx.options.anonymous ? 1 : 0,
+            suggestion: ctx.options.suggestion
+        });
+
+        const suggestChannels = await channelTags.findAll({ where: {
+            guild_id: ctx.guildID,
+            suggest: 1
+        } })
+        const suggestChannelIDs = suggestChannels.map(channel => channel.channel_id);
+        const message = `${ctx.options.anonymous ? '[Anonymous]' : `${ctx.member.user.username}#${ctx.member.user.discriminator}`} suggested: ${ctx.options.suggestion}\nSuggestion ID: ${suggestion.id}`;
+        sendMessages(suggestChannelIDs, message, { split: true });
+        
+        return ctx.send({
+            content: 'suggestion sent',
+            ephemeral: true
+        });
+    }
+}
