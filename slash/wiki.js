eee598889c3494ce04d61bb66db72d27e7abee47
Hexugory <5439333+Hexugory@users.noreply.github.com>
2020-12-19 19:13:38 -0600

    fuck go forward?

--- slash/wiki.js
+++ slash/wiki.js
@@ -38,7 +38,7 @@ module.exports = class WikiCommand extends SlashCommand {
             });
         }
 
-        const extract = await axios.get('https://en.touhouwiki.net/api.php?action=query&prop=extracts&exlimit=1&explaintext=1&exintro=1&redirects&format=json&titles='+search.data.query.search[0].title);
+        const extract = await axios.get('https://en.touhouwiki.net/api.php?action=query&prop=extracts&exlimit=1&explaintext=1&exintro=1&exchars=500&redirects&format=json&titles='+search.data.query.search[0].title);
         
         const page = extract.data.query.pages[Object.keys(extract.data.query.pages)[0]];
 
@@ -47,7 +47,7 @@ module.exports = class WikiCommand extends SlashCommand {
                 new MessageEmbed({
                     title: page.title,
                     url: encodeURI(`https://en.touhouwiki.net/wiki/${page.title}`),
-                    description: page.extract,
+                    description: page.extract.slice(0, -3),
                     footer: {
                         iconURL: `https://en.touhouwiki.net/favicon.ico`,
                         text: `Touhou Wiki`
