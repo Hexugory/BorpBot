c7098f2e72aab3566cb557681af8bd52b0dbddff
James McCall <jimmy.mccall4@gmail.com>
2020-12-19 18:21:48 -0600

    bitchin aesthetics

--- slash/wiki.js
+++ slash/wiki.js
@@ -21,25 +21,29 @@ module.exports = class WikiCommand extends SlashCommand {
     }
 
     async run(ctx) {
-        const search = await axios.get('https://en.touhouwiki.net/api.php', {
-            params: {
-                action: 'query',
-                list: 'search',
-                srsearch: ctx.options.query,
-                srwhat: 'nearmatch',
-                format: 'json'
-            }
-        });
+        const search = await axios.get('https://en.touhouwiki.net/api.php?action=query&prop=extracts&exlimit=1&explaintext=1&exintro=1&redirects&format=json&titles='+ctx.options.query);
         
-        if (!search.data.query.search[0]) {
+        const page = search.data.query.pages[Object.keys(search.data.query.pages)[0]];
+
+        if (!page.pageid) {
             return ctx.send({
                 content: 'that page doesn\'t seem to exist',
                 ephemeral: true
             });
         }
-        
+
         return ctx.send({
-            content: `https://en.touhouwiki.net/wiki/${encodeURIComponent(search.data.query.search[0].title.replace(/ /g, '_'))}`,
+            embeds: [
+                new MessageEmbed({
+                    title: page.title,
+                    url: encodeURI(`https://en.touhouwiki.net/wiki/${page.title}`),
+                    description: page.extract,
+                    footer: {
+                        iconURL: `https://en.touhouwiki.net/favicon.ico`,
+                        text: `Touhou Wiki`
+                    }
+                })
+            ],
             includeSource: true
         });
     }
