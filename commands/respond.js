51a78a602c3d1a8081e1ddc19d7e9be40bef0548
James McCall <jimmy.mccall4@gmail.com>
2021-07-29 14:28:00 -0500

    real shit

--- commands/respond.js
+++ commands/respond.js
@@ -24,8 +24,7 @@ module.exports = {
         const sender = msg.guild.members.resolve(suggestion.sender_id) || (await msg.guild.members.fetch(suggestion.sender_id));
         if (!sender) return msg.reply('that user could not be found')
         sender.send(`The staff of ${msg.guild.name} have sent the following response to one of your suggestions:
-"${args.message}"
-Your name has not been viewed.`, { split: true });
+"${args.message}"${suggestion.anonymous ? '\nYour name has not been viewed.' : ''}`, { split: true });
 
         return msg.reply('response sent');
 	},
